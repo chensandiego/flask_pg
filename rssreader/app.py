9fa7d119021fbaf469699e70bb6f45f2bd17040a
chen <chensandiego@gmail.com>
2017-06-26 14:09:50 +0800

    add loops to display all headlines

--- rssreader/app.py
+++ rssreader/app.py
@@ -1,5 +1,5 @@
 import feedparser
-from flask import Flask
+from flask import Flask,render_template
 
 
 app=Flask(__name__)
@@ -16,12 +16,8 @@ RSS_FEEDS={
 
 def get_news(publication="bbc"):
     feed=feedparser.parse(RSS_FEEDS[publication])
-    first_article=feed['entries'][0]
-    return """<html><body><h1> Headlines</h1>
-            <b>{0}</b><br/><i>{1}</i></br><p>{2}
-            </p></br/>
-            </body>
-            </html>""".format(first_article.get("title"),first_article.get("puublished"),first_article.get("summary"))
+
+    return render_template("home.html",articles=feed['entries'])
 
 
 
